b530a8bbaf9669b47ec40af4b0432ebd832975ed
sarvagya-10 <sarvagyaofficial1@gmail.com>
2025-10-24 09:36:17 +0530

    connect json data

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+import json
+
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as fr:
+        data = json.load(fr)
+    return data
+
 @app.get("/")
-def hello():
-    return{'message':'Hello world'}
+def root():
+    return {'message': 'Hello world'}
 
 @app.get("/name")
-def hello():
-    return{'name':'Sarvagya'}
+def get_name():
+    return {'name': 'Sarvagya'}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
